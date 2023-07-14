65cb19c9c8ff1e1a4e237a01145951dd1ea2c166
Keebs MacBook Pro <will.a.keeble@gmail.com>
2023-07-14 14:52:47 +1000

    create playTurn function

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ function getComputerChoice () {
     const rand = Math.floor(Math.random() * choices.length);
     return choices[rand];
 }
+
+function getPlayerChoice() {
+    const choice = parseInt(prompt("What is your play?\n1 - Rock | 2 - Paper | 3 - Scissors"));
+    return choices[choice-1];
+}
+
+function playRound(playerChoice, computerChoice) {
+    pChoice = playerChoice.toLowerCase();
+    cChoice = computerChoice.toLowerCase();
+    if (pChoice === cChoice) {
+        return "Draw";
+    }
+    case_1 = pChoice === "rock" && cChoice === "paper";
+    case_2 = pChoice === "paper" && cChoice === "scissors";
+    case_3 = pChoice === "scissors" && cChoice === "rock";
+    if (case_1 || case_2 || case_3) {
+        return "You Lose :("
+    }
+    else {
+        return "You Win! :)"
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
